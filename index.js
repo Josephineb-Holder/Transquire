af3b17606dd56a44181f33d5a137f80dbb3d5064
Josephineb-Holder <89983188+Josephineb-Holder@users.noreply.github.com>
2023-08-05 19:35:19 -0400

    Second commit

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const image = document.getElementById("image");
 window.addEventListener("scroll", () => {
   if (window.scrollY > 0) {
     navbar.classList.add("scrolled");
+    navbar.style.animation = "fadeOut 0.1s";
     for (const link of links) {
       link.style.color = "blue";
       image.classList.add("image-style");
     }
   } else {
+    navbar.style.animation = "fadeIn 0.3s";
     navbar.classList.remove("scrolled");
     image.classList.remove("image-style");
     for (const link of links) {
